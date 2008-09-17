2fffe1487736c8b41c9ea086399fb0e349d9be25
mcarter <carter.micah@gmail.com>
2008-09-17 19:33:12 +0000

    Basically, a slightly modified skeleton mod

---
+++ boost/install.sql
@@ -0,0 +1,17 @@
+-- Set up tables for Inventory:
+-- @author Micah Carter <mcarter at tux dot appstate dot edu>
+
+CREATE TABLE inventory_location (
+);
+
+CREATE TABLE inventory_department (
+);
+
+CREATE TABLE inventory_office (
+);
+
+CREATE TABLE inventory_computer (
+);
+
+CREATE TABLE inventory_printer (
+);
