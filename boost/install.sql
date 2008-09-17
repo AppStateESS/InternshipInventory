10947753e05670235374cae3234f3db9dd3fa386
mcarter <carter.micah@gmail.com>
2008-09-17 20:04:04 +0000

    stuff still doesn't work

--- boost/install.sql
+++ boost/install.sql
@@ -1,17 +1,20 @@
 -- Set up tables for Inventory:
 -- @author Micah Carter <mcarter at tux dot appstate dot edu>
 
-CREATE TABLE inventory_location (
+CREATE TABLE sysinventory_location (
 );
 
-CREATE TABLE inventory_department (
+CREATE TABLE sysinventory_department (
 );
 
-CREATE TABLE inventory_office (
+CREATE TABLE sysinventory_office (
 );
 
-CREATE TABLE inventory_computer (
+CREATE TABLE sysinventory_computer (
 );
 
-CREATE TABLE inventory_printer (
+CREATE TABLE sysinventory_printer (
+);
+
+CREATE TABLE sysinventory_employee (
 );
--- boost/uninstall.php
+++ boost/uninstall.php
@@ -1,31 +1,15 @@
 <?php
-/**
-    * skeleton - phpwebsite module
-    *
-    * See docs/AUTHORS and docs/COPYRIGHT for relevant info.
-    *
-    * This program is free software; you can redistribute it and/or modify
-    * it under the terms of the GNU General Public License as published by
-    * the Free Software Foundation; either version 2 of the License, or
-    * (at your option) any later version.
-    * 
-    * This program is distributed in the hope that it will be useful,
-    * but WITHOUT ANY WARRANTY; without even the implied warranty of
-    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    * GNU General Public License for more details.
-    * 
-    * You should have received a copy of the GNU General Public License
-    * along with this program; if not, write to the Free Software
-    * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-    *
-    * @version $Id: uninstall.php 6147 2008-08-27 20:13:56Z matt $
-    * @author Verdon Vaillancourt <verdonv at users dot sourceforge dot net>
-*/
+/*
+ * @author Micah Carter <mcarter at tux dot appstate dot edu>
+ */
+function sysinventory_uninstall(&$content) {
 
-function skeleton_uninstall(&$content) {
-
-    PHPWS_DB::dropTable('skeleton_skeletons');
-    PHPWS_DB::dropTable('skeleton_bones');
+    PHPWS_DB::dropTable('sysinventory_location');
+    PHPWS_DB::dropTable('sysinventory_department');
+    PHPWS_DB::dropTable('sysinventory_office');
+    PHPWS_DB::dropTable('sysinventory_computer');
+    PHPWS_DB::dropTable('sysinventory_printer');
+    PHPWS_DB::dropTable('sysinventory_employee');
     $content[] = dgettext('skeleton', 'Skeleton tables dropped.');
 
     return true;
