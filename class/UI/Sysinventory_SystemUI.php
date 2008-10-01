f0fee31b5e9fe2236581a15609b467fe1961d07b
mcarter <carter.micah@gmail.com>
2008-10-01 01:36:56 +0000

    maintenance commit

--- class/UI/Sysinventory_SystemUI.php
+++ class/UI/Sysinventory_SystemUI.php
@@ -5,6 +5,7 @@
 **/
 
 class Sysinventory_SystemUI {
+
     function showAddSystem() {
         PHPWS_Core::initModClass('sysinventory','Sysinventory_Query.php');
         $dpts = array();
@@ -13,6 +14,9 @@ class Sysinventory_SystemUI {
             $error = "You are not the administrator of any departments.";
             PHPWS_Core::initModClass('sysinventory','Sysinventory_Menu.php');
             Sysinventory_Menu::showMenu($error);
+            return;
         }
+        $form = new PHPWS_Form('add_system');
+        // Add form elements here for adding a new system.
     }
 }
