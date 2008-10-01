e7729e4e0364fccc31653e4797a5092805c4b420
mcarter <carter.micah@gmail.com>
2008-10-01 01:37:23 +0000

    forgot something...

---
+++ class/Sysinventory_Admin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Class defines an administrator from db data
+ * @author Micah Carter <mcarter at tux dot appstate dot edu>
+**/
+
+class Sysinventory_Admin {
+    var id               = NULL;
+    var username         = NULL;
+    var department_id    = NULL;
+
+}
