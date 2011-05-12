fa4d47bc1cda32a5bddb00419bd683609ead851a
bostrt <bostrt@gmail.com>
2011-05-12 14:42:14 -0400

    - Clicking edit link does not expand hider anymore.

--- javascript/hider/hider.js
+++ javascript/hider/hider.js
@@ -11,16 +11,20 @@ var Hider = function(id){
     this.open = false;
     var me = this;
 
-    // Show the details for this internship
-    // and keep the row highlighted.
+    /*
+     * Show the details for this internship
+     * and keep the row highlighted.
+     */
     this.show = function(){
         $(this.dSelect).slideDown('fast');
         $(this.select).css('background', '#E2E2E2');
         this.open = true;
     }
     
-    // Hide the details for this internship
-    // and remove the highlighting.
+    /*
+     * Hide the details for this internship
+     * and remove the highlighting.
+     */
     this.hide = function(){
         $(this.select).css('background', '#FFFFFF');
         $(this.dSelect).slideUp('fast');
@@ -28,22 +32,27 @@ var Hider = function(id){
     }
 
     $(this.select).mouseenter(function(){
-        // Show gray background when mouse if over row
+        /* Show gray background when mouse if over row */
         $(me.select).css('background', '#E2E2E2');
         $(me.select).css('cursor', 'pointer');
     });
 
     $(this.select).mouseleave(function(){
-        // If this hider is opened then keep it highlighted.
+        /* If this hider is opened then keep it highlighted. */
         if(!me.open){
-            // Show white background when mouse if over row
+            /* Show white background when mouse if over row */
             $(me.select).css('background', '#FFFFFF');
         }
     });
-    
-    // Show details if row is clicked...ignore if the 'Edit' link is clicked.
+
+    /* If the Edit link is clicked then stop event propagation. Row isn't expanded. */
+    $(this.select+">.action>a").click(function(event){
+        event.stopPropagation();
+    });
+
+    /* Show details if row is clicked...ignore if the 'Edit' link is clicked. */
     $(this.select).click(function(){
-        // If row is open and is clicked then close it.
+        /* If row is open and is clicked then close it. */
         if(me.open){
             me.hide();
         }else{
@@ -51,7 +60,7 @@ var Hider = function(id){
         }
     });
 
-    // Hide content div and load it with data.
+    /* Hide content div and load it with data. */
     this.hide();
     $.get('index.php', {'module' : 'intern', 'action' : 'internship_details', 'id' : me.id },
           function(data){
