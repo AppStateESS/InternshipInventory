08bf196098d66ecf9a40459b8b63d5a1e3a89416
bostrt <bostrt@gmail.com>
2011-05-18 14:39:45 -0400

    - Added UI for grad programs

---
+++ class/UI/GradProgramUI.php
@@ -0,0 +1,45 @@
+<?php
+
+class GradProgramUI implements UI
+{
+    public static function display()
+    {
+        /* Check if user can add/edit/hide/delete grad programs. */
+        if(!Current_User::allow('intern', 'edit_grad_prog') && 
+           !Current_User::allow('intern', 'delete_grad_prog')){
+            NQ::simple('intern', INTERN_WARNING, 'You do not have permission to edit graduate programs.');
+            return false;
+        }
+
+        $tpl['HOMELINK'] = PHPWS_Text::moduleLink('Back to Menu','intern');
+        $tpl['PAGER'] = self::doPager();
+        
+        /* Form for adding new grad program */
+        $form = &new PHPWS_Form('add_prog');
+        $form->addText('name');
+        $form->setLabel('name', 'Graduate Program Title');
+        $form->addSubmit('submit','Add Graduate Program');
+        $form->setAction('index.php?module=intern&action=edit_grad');
+        $form->addHidden('add',TRUE);
+
+        $form->mergeTemplate($tpl);
+        return PHPWS_Template::process($form->getTemplate(), 'intern', 'edit_grad.tpl');
+    }
+
+    public static function doPager() 
+    {
+        PHPWS_Core::initCoreClass('DBPager.php');
+        PHPWS_Core::initModClass('intern','GradProgram.php');
+
+        $pager = new DBPager('intern_grad_prog', 'GradProgram');
+        $pager->db->addOrder('name asc');
+        $pager->setModule('intern');
+        $pager->setTemplate('grad_pager.tpl');
+        $pager->setEmptyMessage('No Graduate Programs Found.');
+        $pager->addRowTags('getRowTags');
+
+        return $pager->get();
+    }
+}
+
+?>
---
+++ templates/grad_pager.tpl
@@ -0,0 +1,24 @@
+<table id="major-pager">
+<tr>
+    <th>Name</th>
+    <th>Option</th>
+</tr>
+<!-- BEGIN empty_table -->
+<tr>
+    <td colspan=3>{EMPTY_MESSAGE}</td>
+</tr>
+<!-- END empty_table -->
+
+<!-- BEGIN listrows -->
+<tr>
+    <td>{NAME}</td>
+    <td>{EDIT}{HIDE}{DELETE}</td>
+</tr>
+<!-- END listrows -->
+</table>
+
+<div align="center">
+<b>{PAGE_LABEL}</b><br />
+{PAGES}<br />
+{LIMITS}
+</div>
