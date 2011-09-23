bc357c3672284b190346cc07fb9390ee30ed7950
jbooker <jeremy.booker@gmail.com>
2011-09-23 11:14:37 -0400

    Fixed sytling issues on main menu links.

--- class/UI/Intern_MenuUI.php
+++ class/UI/Intern_MenuUI.php
@@ -13,8 +13,8 @@ class Intern_MenuUI implements UI{
         if(isset($_SESSION['query'])) unset($_SESSION['query']);
 
         $tags = array();
-        $tags['SEARCH'] = PHPWS_Text::moduleLink('<img class="menu-icon" src="mod/intern/img/edit-find.png"/> Search Inventory', 'intern', array('action' => 'search'));
-        $tags['ADD']    = PHPWS_Text::moduleLink('<img class="menu-icon" src="mod/intern/img/contact-new.png"/> Add Student', 'intern', array('action' => 'edit_internship'));
+        $tags['SEARCH'] = PHPWS_Text::moduleLink('<h3 class="search-icon">Search Inventory</h3>', 'intern', array('action' => 'search'));
+        $tags['ADD']    = PHPWS_Text::moduleLink('<h3 class="add-icon">Add Student</h3>', 'intern', array('action' => 'edit_internship'));
         $auth = Current_User::getAuthorization();
         $tags['LOGOUT'] = "<a href='$auth->logout_link'>Logout</a>";
 
