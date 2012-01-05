2ca7d61f3816786709399ddcce5b8bb4065fde19
Matt McNaney <matt@tux.appstate.edu>
2012-01-05 09:40:38 -0500

    fixed require path

--- inc/defines.php
+++ inc/defines.php
@@ -4,7 +4,7 @@
 // due to the obvious issue of what to do every time prod is exported.  This
 // file is in subversion is inc/hms_defines.php, and should live in phpWebSite's
 // root as /inc/hms_defines.php.
-require_once(PHPWS_SOURCE_DIR . 'inc/intern_defines.php');
+require_once(PHPWS_SOURCE_DIR . 'mod/intern/inc/intern_defines.php');
 
 
 /**
