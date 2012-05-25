a1eca3c38500ff7ac6c8fc23bec702e92a58da29
jbooker <jeremy.booker@gmail.com>
2012-05-25 11:26:34 -0400

    Database script updates. Refs #108

--- boost/updates/update_0_0_35.sql
+++ boost/updates/update_0_0_35.sql
@@ -2,12 +2,12 @@ ALTER TABLE intern_internship ADD COLUMN banner VARCHAR;
 ALTER TABLE intern_internship ADD COLUMN first_name VARCHAR;
 ALTER TABLE intern_internship ADD COLUMN middle_name VARCHAR;
 ALTER TABLE intern_internship ADD COLUMN last_name VARCHAR;
-ALTER TABLE intern_internship ADD COLUMN gpa VARCHAR NULL;
+ALTER TABLE intern_internship ADD COLUMN gpa VARCHAR;
 ALTER TABLE intern_internship ADD COLUMN level VARCHAR;
 ALTER TABLE intern_internship ADD COLUMN phone VARCHAR;
 ALTER TABLE intern_internship ADD COLUMN email VARCHAR;
-ALTER TABLE intern_internship ADD COLUMN ugrad_major INT NULL REFERENCES intern_major(id);
-ALTER TABLE intern_internship ADD COLUMN grad_prog INT NULL REFERENCES intern_grad_prog(id);
+ALTER TABLE intern_internship ADD COLUMN ugrad_major INT;
+ALTER TABLE intern_internship ADD COLUMN grad_prog INT;
 ALTER TABLE intern_internship ADD COLUMN student_address varchar(256);
 ALTER TABLE intern_internship ADD COLUMN student_city varchar(256);
 ALTER TABLE intern_internship ADD COLUMN student_state varchar(2);
@@ -16,6 +16,10 @@ ALTER TABLE intern_internship ADD COLUMN emergency_contact_name varchar(256);
 ALTER TABLE intern_internship ADD COLUMN emergency_contact_relation varchar(256);
 ALTER TABLE intern_internship ADD COLUMN emergency_contact_phone varchar(20);
 ALTER TABLE intern_internship ADD COLUMN campus character varying(128);
+
+
+ALTER TABLE intern_internship ADD CONSTRAINT ugrad_major_fkey FOREIGN KEY (ugrad_major) REFERENCES intern_major(id);
+ALTER TABLE intern_internship ADD CONSTRAINT grad_prog_fkey FOREIGN KEY (grad_prog) REFERENCES intern_grad_prog(id);
 
 
 UPDATE intern_internship AS i SET banner = s.banner, first_name = s.first_name, middle_name = s.middle_name, last_name = s.last_name, gpa = s.gpa, level = s.level, phone = s.phone, email = s.email, ugrad_major = s.ugrad_major, grad_prog = s.grad_prog, student_address = s.address, student_city = s.city, student_state = s.state, student_zip = s.zip, emergency_contact_name = s.emergency_contact_name, emergency_contact_relation = s.emergency_contact_relation, emergency_contact_phone = s.emergency_contact_phone, campus = s.campus FROM intern_student AS s WHERE i.student_id = s.id;
