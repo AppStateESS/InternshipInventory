41b9f1be483e0ba399957801878a39d04630bb8a
jbooker <jeremy.booker@gmail.com>
2012-10-05 11:53:57 -0400

    Updated emergency contact update script. Refs #88

--- boost/updates/update_00.01.02.sql
+++ boost/updates/update_00.01.02.sql
@@ -9,7 +9,7 @@ CREATE TABLE intern_emergency_contact (
 
 create sequence intern_emergency_contact_seq;
 
-INSERT INTO intern_emergency_contact (id, internship_id, name, relation, phone) SELECT nextval('intern_emergency_contact_seq'), id, emergency_contact_name, emergency_contact_relation, emergency_contact_phone FROM intern_internship WHERE emergency_contact_name IS NOT NULL OR emergency_contact_relation IS NOT NULL OR emergency_contact_phone IS NOT NULL;
+INSERT INTO intern_emergency_contact (id, internship_id, name, relation, phone) SELECT nextval('intern_emergency_contact_seq'), id, emergency_contact_name, emergency_contact_relation, emergency_contact_phone FROM intern_internship WHERE (emergency_contact_name IS NOT NULL OR emergency_contact_relation IS NOT NULL OR emergency_contact_phone IS NOT NULL) AND (emergency_contact_name NOT SIMILAR TO '[ ]*' AND emergency_contact_relation NOT SIMILAR TO '[ ]*' AND emergency_contact_phone NOT SIMILAR TO '[ ]*')
 
 alter table intern_internship drop column emergency_contact_name;
 alter table intern_internship drop column emergency_contact_relation;
