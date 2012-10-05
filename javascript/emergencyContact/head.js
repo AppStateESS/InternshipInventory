832e812b3074afe4c11631c93e1ced668259c8ab
Jeff Tickle <jtickle@tux.appstate.edu>
2012-10-05 12:32:07 -0400

    Fixed emergency contact javascript for branches

--- javascript/emergencyContact/head.js
+++ javascript/emergencyContact/head.js
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="./mod/intern/javascript/emergencyContact/EmgContactList.js" ></script>
+<script type="text/javascript" src="{source_http}mod/intern/javascript/emergencyContact/EmgContactList.js" ></script>
 <script type="text/javascript">
 
 	var emgSpinner = null;
