bf09fa04107e0a21a568a86eaf611632cb38ebf9
jbooker <jeremy.booker@gmail.com>
2013-01-10 09:29:33 -0500

    Fixed distance ed permissions on GraduateRegistration transition. Refs #141

--- class/WorkflowTransitions/GraduateRegistration.php
+++ class/WorkflowTransitions/GraduateRegistration.php
@@ -19,6 +19,21 @@ class GraduateRegistration extends WorkflowTransition {
         }
     }
     
+    public function allowed(Internship $i)
+    {
+    	if($i->isDistanceEd()){
+    		if(Current_User::allow('intern', 'distance_ed_register')){
+    			return true;
+    		}else{
+    			return false;
+    		}
+    	}else{
+    		return parent::allowed($i);
+    	}
+    
+    	return false;
+    }
+    
     public function doNotification(Internship $i, $note = null)
     {
         $agency = $i->getAgency();
