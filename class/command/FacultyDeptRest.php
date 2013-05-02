61dd69a3d69b816cc04e60dc67575084f673e357
jbooker <jeremy.booker@gmail.com>
2013-05-02 09:46:24 -0400

    Add delete functionality for faculty to department relationships. #143

--- class/command/FacultyDeptRest.php
+++ class/command/FacultyDeptRest.php
@@ -13,6 +13,9 @@ class FacultyDeptRest {
             case 'POST':
                 $this->post();
                 exit;
+            case 'DELETE':
+                $this->delete();
+                exit;
             default:
                 header('HTTP/1.1 405 Method Not Allowed');
                 exit;
@@ -41,6 +44,26 @@ class FacultyDeptRest {
         
         echo json_encode($obj);
         
+        exit;
+    }
+    
+    private function delete()
+    {
+        $postArray = json_decode(file_get_contents('php://input'), true);
+        
+        $facultyId       = $postArray['faculty_id'];
+        $departmentId    = $postArray['department_id'];
+        
+        $sql = "DELETE FROM intern_faculty_department WHERE faculty_id = $facultyId AND department_id = $departmentId";
+
+        $result = PHPWS_DB::query($sql);
+        
+        if(PHPWS_Error::logIfError($result)){
+            header('HTTP/1.1 500 Internal Server Error');
+            exit;
+        }
+        
+        header('HTTP/1.1 204 No Content');
         exit;
     }
 }
