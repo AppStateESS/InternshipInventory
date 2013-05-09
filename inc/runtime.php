b340a53fcb10e25fb111d3b19170aa2d56ec184e
jbooker <jeremy.booker@gmail.com>
2013-05-09 15:51:15 -0400

    Trying a fix for the authenticated by no account problem.

--- inc/runtime.php
+++ inc/runtime.php
@@ -1,5 +1,10 @@
 <?php
 
+if(!Current_User::isLogged() && isset($_SERVER['HTTP_SHIB_EP_PRINCIPALNAME'])) {
+    NQ::simple('intern', INTERN_ERROR, "You have successfully signed in, but we have not setup your Internship Inventory account. Please contact the Career Development Center at 828-262-2180.");
+    NQ::close();
+}
+
 if (PHPWS_Core::atHome() && Current_User::isLogged()) {
     $path = $_SERVER['SCRIPT_NAME'].'?module=intern';
 
