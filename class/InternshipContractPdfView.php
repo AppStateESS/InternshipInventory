80b0acd19b0f972d2cdbd5369be6800b7a07e0e6
Jeremy Booker <jeremy.booker@gmail.com>
2014-06-10 15:57:05 -0400

    Updated internship contract.

--- class/InternshipContractPdfView.php
+++ class/InternshipContractPdfView.php
@@ -58,7 +58,7 @@ class InternshipContractPdfView {
         $g = $this->internship->getGradProgram();
         $subject = $this->internship->getSubject();
 
-        $pagecount = $this->pdf->setSourceFile(PHPWS_SOURCE_DIR . 'mod/intern/pdf/contract-flat.pdf');
+        $pagecount = $this->pdf->setSourceFile(PHPWS_SOURCE_DIR . 'mod/intern/pdf/ASU_Internship_Contract_101711_v7c-5-28-14.pdf');
         $tplidx = $this->pdf->importPage(1);
         $this->pdf->addPage();
         $this->pdf->useTemplate($tplidx);
@@ -80,12 +80,12 @@ class InternshipContractPdfView {
 
         // Subject and Course #
         //$this->pdf->setFont('Times', null, 8);
-        $this->pdf->setXY(132, 44);
+        $this->pdf->setXY(132, 49);
         $course_info = $this->internship->getCourseNumber();
         $this->pdf->cell(59, 5, $course_info);
 
         // Section #
-        $this->pdf->setXY(178, 44);
+        $this->pdf->setXY(178, 49);
         $this->pdf->cell(25, 5, $this->internship->getCourseSection());
 
         /*
@@ -104,59 +104,59 @@ class InternshipContractPdfView {
 
         /* Location */
         if($this->internship->isDomestic()){
-            $this->pdf->setXY(85, 62);
+            $this->pdf->setXY(85, 64);
             $this->pdf->cell(12, 5, 'X');
         }
         if($this->internship->isInternational()){
-            $this->pdf->setXY(156, 62);
+            $this->pdf->setXY(156, 64);
             $this->pdf->cell(12, 5, 'X');
         }
 
         /**
          * Student information.
          */
-        $this->pdf->setXY(40, 77);
+        $this->pdf->setXY(40, 81);
         $this->pdf->cell(55, 5, $this->internship->getFullName());
 
-        $this->pdf->setXY(173,77);
+        $this->pdf->setXY(173,81);
         $this->pdf->cell(54,5, $this->internship->getGpa());
 
-        $this->pdf->setXY(32, 83);
+        $this->pdf->setXY(32, 87);
         $this->pdf->cell(42, 5, $this->internship->getBannerId());
 
-        $this->pdf->setXY(41, 88);
+        $this->pdf->setXY(41, 92);
         $this->pdf->cell(54, 5, $this->internship->getEmailAddress() . '@appstate.edu');
 
-        $this->pdf->setXY(113, 88);
+        $this->pdf->setXY(113, 92);
         $this->pdf->cell(54, 5, $this->internship->getPhoneNumber());
 
         /* Student Address */
-        $this->pdf->setXY(105, 83);
+        $this->pdf->setXY(105, 87);
         $this->pdf->cell(54, 5, $this->internship->getStudentAddress());
 
 
         /* Payment */
         if($this->internship->isPaid()){
-            $this->pdf->setXY(160, 88);
+            $this->pdf->setXY(160, 92);
             $this->pdf->cell(10,5, 'X');
         }
 
         if($this->internship->isUnPaid()){
-            $this->pdf->setXY(190, 88);
+            $this->pdf->setXY(190, 92);
             $this->pdf->cell(10,5,'X');
         }
 
         /* Start/end dates */
         //$this->pdf->setFont('Times', null, 10);
-        $this->pdf->setXY(50, 93);
+        $this->pdf->setXY(50, 97);
         $this->pdf->cell(25, 5, $this->internship->getStartDate(true));
-        $this->pdf->setXY(93, 93);
+        $this->pdf->setXY(93, 97);
         $this->pdf->cell(25, 5, $this->internship->getEndDate(true));
 
         /* Hours */
-        $this->pdf->setXY(193, 93);
+        $this->pdf->setXY(193, 97);
         $this->pdf->cell(12, 5, $this->internship->getCreditHours());
-        $this->pdf->setXY(157, 93);
+        $this->pdf->setXY(157, 97);
         $this->pdf->cell(12, 5, $this->internship->getAvgHoursPerWeek()); // hours per week
 
         //        $this->pdf->cell(35, 5, 'Graduate Program:', 'LTB');
@@ -171,38 +171,38 @@ class InternshipContractPdfView {
          * Faculty supervisor information.
          */
         if(isset($f)){
-            $this->pdf->setXY(26, 109);
+            $this->pdf->setXY(26, 118);
             $this->pdf->cell(81, 5, $f->getFullName());
 
-            $this->pdf->setXY(29, 116);
+            $this->pdf->setXY(29, 125);
             $this->pdf->cell(81, 5, $f->getStreetAddress1());
 
-            $this->pdf->setXY(15, 123);
+            $this->pdf->setXY(15, 132);
             $this->pdf->cell(81, 5, $f->getStreetAddress2());
             
-            $this->pdf->setXY(60, 123);
+            $this->pdf->setXY(60, 132);
             $this->pdf->cell(81, 5, $f->getCity());
             
-            $this->pdf->setXY(88, 123);
+            $this->pdf->setXY(88, 132);
             $this->pdf->cell(81, 5, $f->getState());
             
-            $this->pdf->setXY(95, 123);
+            $this->pdf->setXY(95, 132);
             $this->pdf->cell(81, 5, $f->getZip());
 
-            $this->pdf->setXY(26, 131);
+            $this->pdf->setXY(26, 139);
             $this->pdf->cell(77, 5, $f->getPhone());
 
-            $this->pdf->setXY(25, 138);
+            $this->pdf->setXY(25, 146);
             $this->pdf->cell(77, 5, $f->getFax());
 
-            $this->pdf->setXY(26, 145);
+            $this->pdf->setXY(26, 154);
             $this->pdf->cell(77, 5, $f->getUsername() . '@appstate.edu');
         }
 
         /***
          * Agency information.
         */
-        $this->pdf->setXY(133, 108);
+        $this->pdf->setXY(133, 116);
         $this->pdf->cell(71, 5, $a->getName());
 
         $agency_address = $a->getAddress();
@@ -210,23 +210,23 @@ class InternshipContractPdfView {
         //TODO: make this smarter so it adds the line break between words
         if(strlen($agency_address) < 49){
             // If it's short enough, just write it
-            $this->pdf->setXY(125, 114);
+            $this->pdf->setXY(125, 121);
             $this->pdf->cell(77, 5, $agency_address);
         }else{
             // Too long, need to use two lines
             $agencyLine1 = substr($agency_address, 0, 49); // get first 50 chars
             $agencyLine2 = substr($agency_address, 49); // get the rest, hope it fits
 
-            $this->pdf->setXY(125, 114);
+            $this->pdf->setXY(125, 121);
             $this->pdf->cell(77, 5, $agencyLine1);
-            $this->pdf->setXY(110, 118);
+            $this->pdf->setXY(110, 126);
             $this->pdf->cell(77, 5, $agencyLine2);
         }
 
         /**
          * Agency supervisor info.
          */
-        $this->pdf->setXY(110, 129);
+        $this->pdf->setXY(110, 136);
         $super = "";
         $superName = $a->getSupervisorFullName();
         if(isset($superName) && !empty($superName) && $superName != ''){
@@ -241,16 +241,16 @@ class InternshipContractPdfView {
         }
         $this->pdf->cell(75, 5, $super);
 
-        $this->pdf->setXY(124, 134);
+        $this->pdf->setXY(124, 142);
         $this->pdf->cell(78, 5, $a->getSuperAddress());
 
-        $this->pdf->setXY(122, 144);
+        $this->pdf->setXY(122, 152);
         $this->pdf->cell(72, 5, $a->getSupervisorEmail());
 
-        $this->pdf->setXY(122, 139);
+        $this->pdf->setXY(122, 147);
         $this->pdf->cell(33, 5, $a->getSupervisorPhoneNumber());
 
-        $this->pdf->setXY(163, 139);
+        $this->pdf->setXY(163, 147);
         $this->pdf->cell(40, 5, $a->getSupervisorFaxNumber());
 
         /* Internship Location */
