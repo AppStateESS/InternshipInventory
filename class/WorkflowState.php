f3ff916deb562fb4ea65d20932534be5d971c48c
Jeremy Booker <jeremy.booker@gmail.com>
2014-07-18 15:49:03 -0400

    Set default workflow transition on internship form.

--- class/WorkflowState.php
+++ class/WorkflowState.php
@@ -6,7 +6,7 @@ abstract class WorkflowState {
 
     const friendlyName = '';
     const sortIndex    = 5;
-    
+
     /**
      * Returns an array of the valid WorkflowActions for this State.
      * @return Array<WorkflowAction>
@@ -15,32 +15,32 @@ abstract class WorkflowState {
     {
         return WorkflowTransitionFactory::getTransitionsFromState($this, $i);
     }
-    
+
     /**
      * Called when an action causes this state to become the current state.
      */
     public function onEnter(WorkflowAction $action)
     {
-        
+
     }
-    
+
     /**
-     * After an action occurs, this is called just before leaving this state. 
+     * After an action occurs, this is called just before leaving this state.
      */
     public function onExit(WorkflowAction $action)
     {
-        
+
     }
-    
+
     public function getName(){
         return get_called_class();
     }
-    
+
     public function getFriendlyName(){
         $class = $this->getName();
         return $class::friendlyName;
     }
-    
+
     public function getSortIndex()
     {
         $class = get_called_class();
