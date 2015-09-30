9d126d1f14807b07e24c0359a9795e80dad31e98
Jeremy Booker <jeremy.booker@gmail.com>
2015-09-30 10:28:49 -0400

    Remove stray var_dump.

--- class/EditInternshipFormView.php
+++ class/EditInternshipFormView.php
@@ -486,7 +486,6 @@ class EditInternshipFormView {
         $birthday = $this->intern->getBirthDateFormatted();
 
         if(is_null($birthday)) {
-            var_dump('in here');
             $this->tpl['BIRTH_DATE'] = '<span class="text-muted"><em>Not available</em></span>';
         } else {
             $this->tpl['BIRTH_DATE'] = $this->intern->getBirthDateFormatted();
