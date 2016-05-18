dc4fbb460bb88b3353279b6c48318f49632f2872
Eric Cambel <cambelem@appstate.edu>
2016-05-18 10:41:44 -0400

    Fixed bug when creating an admin on a new department.

--- javascript/editAdmin/head.js
+++ javascript/editAdmin/head.js
@@ -1,4 +1,4 @@
 
-<script src="https://fb.me/react-0.13.1.js"></script>
+<script src="https://fb.me/react-with-addons-0.13.1.js"></script>
 <script src="https://fb.me/JSXTransformer-0.13.1.js"></script>
 <script type ="text/jsx" src = "{source_http}mod/intern/javascript/editAdmin/editAdmin.jsx"></script>
