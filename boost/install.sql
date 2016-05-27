255326c7df69e74d75ae8eb222645642f9eff493
Jeremy Booker <jeremy.booker@gmail.com>
2016-05-27 11:53:29 -0400

    Fix sql syntax error in install.sql

--- boost/install.sql
+++ boost/install.sql
@@ -590,8 +590,8 @@ CREATE TABLE intern_internship (
        multi_part SMALLINT,
        secondary_part SMALLINT,
        experience_type varchar default 'internship',
-       background_check SMALLINT DEFAULT 0;
-       drug_check SMALLINT DEFAULT 0;
+       background_check SMALLINT DEFAULT 0,
+       drug_check SMALLINT DEFAULT 0,
        PRIMARY KEY(id)
 );
 
