fa01b736b8d6596c461f307c19a70e52ce2a1881
William Asciutto <wjasciutto@gmail.com>
2016-07-14 10:20:58 -0400

    Add update 1.15 to drop not null constraint from 4 internship fields that prevented creation of new internship.

---
+++ boost/updates/update_00.01.15.sql
@@ -0,0 +1,4 @@
+alter table intern_internship alter start_date drop not null;
+alter table intern_internship alter end_date drop not null;
+alter table intern_internship alter background_check drop not null;
+alter table intern_internship alter drug_check drop not null;
