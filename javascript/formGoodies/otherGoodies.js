cf5f1bd7d65de96290554d37036a5bc2f6823486
William Asciutto <wjasciutto@gmail.com>
2016-07-15 10:35:38 -0400

    Add modal to internshipView. Remove previous js implementation.

--- javascript/formGoodies/otherGoodies.js
+++ javascript/formGoodies/otherGoodies.js
@@ -94,17 +94,37 @@ function initTypeHelp()
      * Internship Type Help *
      */
     // Setup the dialog
-    $("#internship-type-help").dialog({
-        autoOpen: false,
-        buttons: {Close: function() {$(this).dialog("close");}},
-        modal: true,
-        width: 600
-    });
+    // $("#internship-type-help").dialog({
+    //     autoOpen: false,
+    //     buttons: {Close: function() {$(this).dialog("close");}},
+    //     modal: true,
+    //     width: 600
+    // });
+    //
+    // // Register the event handler for the help button that shows the dialog
+    // $("#internship-type-help-button").click(function(){
+    //     $("#internship-type-help").dialog('open');
+    // });
+    <!-- Modal -->
+    <div id ="typeModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+      exit;
+        <!-- Modal content -->
+        <div class = "modal-content">
+          <div class = "modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Modal Header</h4>
+          </div>
+          <div class="modal-body">
+            <p>Some text in the modal.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
 
-    // Register the event handler for the help button that shows the dialog
-    $("#internship-type-help-button").click(function(){
-        $("#internship-type-help").dialog('open');
-    });
+      </div>
+    </div>
 }
 
 function initMultiPart()
