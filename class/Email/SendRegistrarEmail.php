eef3a68dff3ef7ed6493f9b0b302f58874fb0a3e
William Asciutto <wjasciutto@gmail.com>
2016-07-20 11:26:39 -0400

    Refactor SendRegistrarEmail to have fields.

--- class/Email/SendRegistrarEmail.php
+++ class/Email/SendRegistrarEmail.php
@@ -12,113 +12,111 @@ class SendRegistrarEmail extends Email {
   * @param Agency $a
   */
   public function __construct(Internship $i, Agency $a) {
-    sendSpecialMessage($i, $a);
+    self::sendSpecialMessage($i, $a);
   }
 
   public function setUpSpecial() {
-    $subjects = Subject::getSubjects();
+    $faculty = $this->internship->getFaculty();
 
-    $faculty = $i->getFaculty();
-
-    if(isset($i->course_subj)){
-      $tpl['SUBJECT'] = $subjects[$i->course_subj];
+    if(isset($this->internship->course_subj)){
+      $this->tpl['SUBJECT'] = $this->subjects[$this->internship->course_subj];
     }else{
-      $tpl['SUBJECT'] = '(No course subject provided)';
+      $this->tpl['SUBJECT'] = '(No course subject provided)';
     }
-    $tpl['COURSE_NUM'] = $i->course_no;
+    $this->tpl['COURSE_NUM'] = $this->internship->course_no;
 
-    if(isset($i->course_sect)){
-      $tpl['SECTION'] = $i->course_sect;
+    if(isset($this->internship->course_sect)){
+      $this->tpl['SECTION'] = $this->internship->course_sect;
     }else{
-      $tpl['SECTION'] = '(not provided)';
+      $this->tpl['SECTION'] = '(not provided)';
     }
 
-    if(isset($i->course_title)){
-      $tpl['COURSE_TITLE'] = $i->course_title;
+    if(isset($this->internship->course_title)){
+      $this->tpl['COURSE_TITLE'] = $this->internship->course_title;
     }
 
-    if(isset($i->credits)){
-      $tpl['CREDITS'] = $i->credits;
+    if(isset($this->internship->credits)){
+      $this->tpl['CREDITS'] = $this->internship->credits;
     }else{
-      $tpl['CREDITS'] = '(not provided)';
+      $this->tpl['CREDITS'] = '(not provided)';
     }
 
-    $startDate = $i->getStartDate(true);
+    $startDate = $this->internship->getStartDate(true);
     if(isset($startDate)){
-      $tpl['START_DATE'] = $startDate;
+      $this->tpl['START_DATE'] = $startDate;
     }else{
-      $tpl['START_DATE'] = '(not provided)';
+      $this->tpl['START_DATE'] = '(not provided)';
     }
 
-    $endDate = $i->getEndDate(true);
+    $endDate = $this->internship->getEndDate(true);
     if(isset($endDate)){
-      $tpl['END_DATE'] = $endDate;
+      $this->tpl['END_DATE'] = $endDate;
     }else{
-      $tpl['END_DATE'] = '(not provided)';
+      $this->tpl['END_DATE'] = '(not provided)';
     }
 
     if($faculty instanceof Faculty){
-      $faculty = $i->getFaculty();
-      $tpl['FACULTY'] = $faculty->getFullName() . ' ('
+      $faculty = $this->internship->getFaculty();
+      $this->tpl['FACULTY'] = $faculty->getFullName() . ' ('
       . $faculty->getId() . ')';
     }else{
-      $tpl['FACULTY'] = '(not provided)';
+      $this->tpl['FACULTY'] = '(not provided)';
     }
 
-    $department = $i->getDepartment();
-    $tpl['DEPT'] = $department->getName();
+    $department = $this->internship->getDepartment();
+    $this->tpl['DEPT'] = $department->getName();
 
-    $campus = $i->getCampus();
+    $campus = $this->internship->getCampus();
     if ($campus == 'distance_ed') {
-      $tpl['CAMPUS'] = 'Distance Ed';
+      $this->tpl['CAMPUS'] = 'Distance Ed';
     } else if ($campus == 'main_campus') {
-      $tpl['CAMPUS'] = 'Main campus';
+      $this->tpl['CAMPUS'] = 'Main campus';
     } else {
-      $tpl['CAMPUS'] = $campus;
+      $this->tpl['CAMPUS'] = $campus;
     }
 
     /**** Corequisite Checking ****/
-    $coreq = $i->getCorequisiteNum();
+    $coreq = $this->internship->getCorequisiteNum();
     if (!is_null($coreq) && $coreq != '') {
-      $tpl['COREQ_SUBJECT'] = $subjects[$i->course_subj];
-      $tpl['COREQ_COURSE_NUM'] = $coreq;
-      $tpl['COREQ_COURSE_SECT'] = $i->getCorequisiteSection();
+      $this->tpl['COREQ_SUBJECT'] = $this->subjects[$this->internship->course_subj];
+      $this->tpl['COREQ_COURSE_NUM'] = $coreq;
+      $this->tpl['COREQ_COURSE_SECT'] = $this->internship->getCorequisiteSection();
     }
 
     /**** International Checking ***/
-    if ($i->international) {
-      $tpl['COUNTRY'] = $i->loc_country;
-      $tpl['INTERNATIONAL'] = 'Yes';
+    if ($this->internship->international) {
+      $this->tpl['COUNTRY'] = $this->internship->loc_country;
+      $this->tpl['INTERNATIONAL'] = 'Yes';
       $intlSubject = '[int\'l] ';
     } else {
-      $tpl['STATE'] = $i->loc_state;
-      $tpl['INTERNATIONAL'] = 'No';
+      $this->tpl['STATE'] = $this->internship->loc_state;
+      $this->tpl['INTERNATIONAL'] = 'No';
       $intlSubject = '';
     }
 
     /**** Multi-part checking ***/
-    if ($i->isMultipart() && $i->isSecondaryPart()) {
-      $tpl['SECONDARY_PART'] = '';
+    if ($this->internship->isMultipart() && $this->internship->isSecondaryPart()) {
+      $this->tpl['SECONDARY_PART'] = '';
     }
 
     /***
     * Figure out who the notification email goes to
     */
     // Send distance ed internship to speedse, per trac #110
-    if ($i->isDistanceEd()) {
-      $to = $settings->getDistanceEdEmail();
+    if ($this->internship->isDistanceEd()) {
+      $this->to = $settings->getDistanceEdEmail();
 
       // Send all international or graduate internships to
       //  'hicksmp', per trac #102
-    } else if ($i->isInternational() || $i->isGraduate()) {
-      $to = $settings->getGraduateRegEmail();
+    } else if ($this->internship->isInternational() || $this->internship->isGraduate()) {
+      $this->to = $settings->getGraduateRegEmail();
 
       // Otherwise, send it to the general Registrar address
     } else {
-      $to = $settings->getRegistrarEmail();
+      $this->to = $settings->getRegistrarEmail();
     }
 
-    if(!isset($to) || $to == null) {
+    if(!isset($this->to) || $this->to == null) {
       throw new \InvalidArgumentException('Missing configurating for email
       addresses (registrar)');
     }
@@ -130,12 +128,9 @@ class SendRegistrarEmail extends Email {
       $cc = array();
     }
 
-    $ouputs = array();
-    $outputs['to'] = $to;
-    $outputs['subject'] = $term . ' ' . $intlSubject . '[' . $i->getBannerId()
-    . '] ' . $i->getFullName();
-    $outputs['doc'] = 'email/RegistrarEmail.tpl';
-    $outputs['cc'] = $cc;
-    return $outputs;
+    $this->subjects = $this->tpl['TERM'] . ' ' . $intlSubject . '[' . $this->internship->getBannerId()
+    . '] ' . $this->internship->getFullName();
+    $this->doc = 'email/RegistrarEmail.tpl';
+    $this->cc = $cc;
   }
 }
