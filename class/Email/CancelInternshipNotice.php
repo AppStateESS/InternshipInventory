47ecd791c988a4ce882477e6260cbb99a1ce482a
Cydney Caldwell <caldwellc1@appstate.edu>
2017-06-27 13:02:17 -0400

    add distance and international

--- class/Email/CancelInternshipNotice.php
+++ class/Email/CancelInternshipNotice.php
@@ -43,18 +43,28 @@ class CancelInternshipNotice extends Email {
         $dept = new Department($this->internship->department_id);
         $this->tpl['DEPARTMENT'] = $dept->getName();
 
+        //email student
         $this->to = $this->internship->email . $this->emailSettings->getEmailDomain();
 
+        //CC faculty
         $faculty = $this->internship->getFaculty();
         if ($faculty instanceof Faculty) {
             $this->cc[] = $faculty->getUsername() . $this->emailSettings->getEmailDomain();
         }
 
-        // CC the graduate school (for grad level) or the registrar's office (for undergrad level)
-        if($this->internship->isGraduate()){
+        // CC the graduate school (for grad level), distance ed, international, or the registrar's office (for undergrad level)
+        if($this->internship->isDistanceEd()){
+            $this->cc[] = $this->emailSettings->getDistanceEdEmail();
+            $this->tpl['UNDERGRAD'] = ''; // Dummy template var to use undergrad text
+        } else if($this->internship->isGraduate()){
             $this->cc[] = explode(',', $this->emailSettings->getGraduateRegEmail()); // NB: Setting is a comma separated array
+            $this->tpl['GRADUATE'] = ''; // Dummy template var to use grad school text
+        } else if($this->internship->isInternational()){
+            $this->cc[] = $this->emailSettings->getInternationalRegEmail();
+            $this->tpl['UNDERGRAD'] = ''; // Dummy template var to use undergrad text
         } else {
             $this->cc[] = $this->emailSettings->getRegistrarEmail();
+            $this->tpl['UNDERGRAD'] = ''; // Dummy template var to use undergrad text
         }
 
         $this->subject = 'Internship Cancelled ' . Term::rawToRead($this->internship->getTerm()) . '[' . $this->internship->getBannerId() . '] ' . $this->internship->getFullName();
