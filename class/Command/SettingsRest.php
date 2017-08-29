2bb4ade9d5a1bdadcfa6c5c54b88d46a70e3ae65
Mollie Gentry <gentryme@essdev3.ess.appstate.edu>
2017-08-29 16:43:45 -0400

    settings update works, cosmetic things and notification left

--- class/Command/SettingsRest.php
+++ class/Command/SettingsRest.php
@@ -6,14 +6,15 @@ class SettingsRest {
 
     public function execute(){
 
-        // if(!\Current_User::allow('intern', 'affiliation_agreement')){
-        //     \NQ::simple('intern', \Intern\UI\NotifyUI::WARNING, 'You do not have permission to add Affiliation Agreements.');
-        //     throw new \Intern\Exception\PermissionException('You do not have permission to add Affiliation Agreements.');
-        // }
+        if(!\Current_User::allow('intern', 'affiliation_agreement')){
+            \NQ::simple('intern', \Intern\UI\NotifyUI::WARNING, 'You do not have permission to add Affiliation Agreements.');
+            throw new \Intern\Exception\PermissionException('You do not have permission to add Affiliation Agreements.');
+        }
 
         switch($_SERVER['REQUEST_METHOD']) {
             case 'POST':
                 $this->post();
+                echo (json_encode("Nice"));
                 exit;
             case 'GET':
                 $data = $this->get();
@@ -26,19 +27,20 @@ class SettingsRest {
     }
 
     public function post(){
-        PHPWS_Settings::set('intern','systemName',$_REQUEST['systemName']);
-        PHPWS_Settings::set('intern','registrarEmail',$_REQUEST['registrarEmail']);
-        PHPWS_Settings::set('intern','distanceEdEmail',$_REQUEST['distanceEdEmail']);
-        PHPWS_Settings::set('intern','internationalRegEmail',$_REQUEST['internationalRegEmail']);
-        PHPWS_Settings::set('intern','graduateRegEmail',$_REQUEST['graduateRegEmail']);
-        PHPWS_Settings::set('intern','gradSchoolEmail',$_REQUEST['gradSchoolEmail']);
-        PHPWS_Settings::set('intern','internationalOfficeEmail',$_REQUEST['internationalOfficeEmail']);
-        PHPWS_Settings::set('intern','fromEmail',$_REQUEST['fromEmail']);
-        PHPWS_Settings::set('intern','emailDomain',$_REQUEST['emailDomain']);
-        PHPWS_Settings::set('intern','backgroundCheckEmail',$_REQUEST['backgroundCheckEmail']);
-        PHPWS_Settings::set('intern','wsdlUri',$_REQUEST['wsdlUri']);
+        \PHPWS_Settings::set('intern','systemName',$_REQUEST['systemName']);
+        \PHPWS_Settings::set('intern','registrarEmail',$_REQUEST['registrarEmail']);
+        \PHPWS_Settings::set('intern','distanceEdEmail',$_REQUEST['distanceEdEmail']);
+        \PHPWS_Settings::set('intern','internationalRegEmail',$_REQUEST['internationalRegEmail']);
+        \PHPWS_Settings::set('intern','graduateRegEmail',$_REQUEST['graduateRegEmail']);
+        \PHPWS_Settings::set('intern','gradSchoolEmail',$_REQUEST['gradSchoolEmail']);
+        \PHPWS_Settings::set('intern','internationalOfficeEmail',$_REQUEST['internationalOfficeEmail']);
+        \PHPWS_Settings::set('intern','fromEmail',$_REQUEST['fromEmail']);
+        \PHPWS_Settings::set('intern','emailDomain',$_REQUEST['emailDomain']);
+        \PHPWS_Settings::set('intern','backgroundCheckEmail',$_REQUEST['backgroundCheckEmail']);
+        \PHPWS_Settings::set('intern','wsdlUri',$_REQUEST['wsdlUri']);
+        \PHPWS_Settings::set('intern','unusualCourseEmail',$_REQUEST['unusualCourseEmail']);
 
-        PHPWS_Settings::save('intern');
+        \PHPWS_Settings::save('intern');
     }
 
     public function get(){
--- javascript/settings/settings.jsx
+++ javascript/settings/settings.jsx
@@ -2,18 +2,47 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
+class ErrorMessagesBlock extends Component{
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    render() {
+        // if(this.props.errorSet == null){
+        //     return '';
+        // }
+
+        //var errors = this.props.errorSet;
+
+        return (
+            <div className="row">
+                <div className="col-sm-12 col-md-6 col-md-push-3">
+                    <div className={"alert alert-" + this.props.stat} role="alert">
+                        <p><i className="fa fa-exclamation-circle fa-2x"></i> Success: {this.props.notif}</p>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
+
 class Settings extends Component {
     constructor(props) {
         super(props);
         this.state = {
             data: null,
-            submitted: false
+            submitted: false,
+            notification: null,
+            notificationStatus: null
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit(e) {
+        e.preventDefault();
+
         var data = {systemName: this.systemNameInput.value,
                     registrarEmail: this.registrarEmailInput.value,
                     gradSchoolEmail: this.gradSchoolEmailInput.value,
@@ -24,22 +53,30 @@ class Settings extends Component {
                     emailDomain: this.emailDomainInput.value,
                     internationalOfficeEmail: this.internationalOfficeEmailInput.value,
                     wsdlUri: this.wsdlUriInput.value,
-                    fromEmail: this.fromEmailInput.value};
+                    fromEmail: this.fromEmailInput.value,
+                    unusualCourseEmail: this.unusualCourseEmailInput.value};
 
-        $.ajax({
-            url: 'index.php?module=intern&action=settingsRest',
-            type: 'POST',
-            dataType: 'json',
-            data: data,
-            success: function() {
-                alert("Updated...");
-                this.setState({data: data});
-            }.bind(this)
+        this.setState({submitted: true}, function(){
+            $.ajax({
+                url: 'index.php?module=intern&action=settingsRest',
+                type: 'POST',
+                data: data,
+                dataType: 'json',
+                success: function() {
+                    var message = "Settings have been updated.";
+                    var notifStatus = "success";
+                    this.setState({data: data, submitted: false, notification: message, notificationStatus: notifStatus});
+                }.bind(this),
+                error: function(xhr, status, err) {
+    				var message = "Settings did not save.";
+                    var notifStatus = "danger";
+                    this.setState({notification: message, notificationStatus: notifStatus});
+    				console.error(this.props.url, status, err.toString());
+    			}.bind(this)
+            });
         });
 
-        this.setState({submitted: true});
-        e.preventDefault();
-
+        console.log(data);
     }
 
     componentDidMount() {
@@ -51,7 +88,9 @@ class Settings extends Component {
 				this.setState({data: data});
 			}.bind(this),
 			error: function(xhr, status, err) {
-				alert("Failed to grab displayed data.")
+				var message = "Failed to grab settings data.";
+                var notifStatus = "danger";
+                this.setState({errorMessage: message, notificationStatus: notifStatus});
 				console.error(this.props.url, status, err.toString());
 			}.bind(this)
 		});
@@ -71,57 +110,73 @@ class Settings extends Component {
             button = <button type="submit" className="btn btn-lg btn-primary pull-right" id="create-btn" onClick={this.handleSubmit} >Save Settings</button>;
         }
 
+        var errorSet;
+        if(this.state.notification === null){
+            errorSet = '';
+        } else {
+            errorSet = <ErrorMessagesBlock key="notification" notif={this.state.notification} stat={this.state.notificationStatus}/>
+        }
+
         return (
-            <div> <h1> Admin Settings </h1>
+            <div className="container">
+            {errorSet}
+            <h1> Admin Settings </h1><br/>
             <form onSubmit={this.handleSubmit} >
-                <div className="form-group">
+                <div className="form-group col-md-6">
                     <label htmlFor="systemName">System Name:</label>
                     <input className="form-control" type="text" defaultValue={this.state.data.systemName} id="systemName" name="systemName" ref={input => this.systemNameInput = input} ></input>
                 </div>
-                <div className="form-group">
-                    <label>Registrar Email:</label>
-                    <input className="form-control" type="text" defaultValue={this.state.data.registrarEmail} id="registrarEmail" name="registrarEmail" ref={input => this.registrarEmailInput = input}></input>
-                </div>
-                <div className="form-group">
-                    <label>Grad School Email:</label>
-                    <input className="form-control" type="text" defaultValue={this.state.data.gradSchoolEmail} id="gradSchoolEmail" name="gradSchoolEmail" ref={input => this.gradSchoolEmailInput = input}></input>
-                </div>
-                <div className="form-group">
-                    <label>Background Check Email:</label>
-                    <input className="form-control" type="text" defaultValue={this.state.data.backgroundCheckEmail} id="backgroundCheckEmail" name="backgroundCheckEmail" ref={input => this.backgroundCheckEmailInput = input}></input>
-                </div>
-                <div className="form-group">
-                    <label>Graduate Registrar Email:</label>
-                    <input className="form-control" type="text" defaultValue={this.state.data.graduateRegEmail} id="gradRegistrarEmail" name="gradRegistrarEmail" ref={input => this.gradRegistrarEmailInput = input}></input>
-                </div>
-                <div className="form-group">
-                    <label>International Registrar Email:</label>
-                    <input className="form-control" type="text" defaultValue={this.state.data.internationalRegEmail} id="internationalRegistrarEmail" name="internationalRegistrarEmail" ref={input => this.internationalRegistrarEmailInput = input}></input>
-                </div>
-                <div className="form-group">
-                    <label>Distance Education Email:</label>
-                    <input className="form-control" type="text" defaultValue={this.state.data.distanceEdEmail} id="distanceEdEmail" name="distanceEdEmail" ref={input => this.distanceEdEmailInput = input}></input>
-                </div>
-                <div className="form-group">
-                    <label>Email Domain:</label>
-                    <input className="form-control" type="text" defaultValue={this.state.data.emailDomain} id="emailDomain" name="emailDomain" ref={input => this.emailDomainInput = input}></input>
-                </div>
-                <div className="form-group">
-                    <label>International Office Email:</label>
-                    <input className="form-control" type="text" defaultValue={this.state.data.internationalOfficeEmail} id="internationalOfficeEmail" name="internationalOfficeEmail" ref={input => this.internationalOfficeEmailInput = input}></input>
-                </div>
-                <div className="form-group">
+                <div className="form-group col-md-6">
                     <label>wsdlUri:</label>
                     <input className="form-control" type="text" defaultValue={this.state.data.wsdlUri} id="wsdlUri" name="wsdlUri" ref={input => this.wsdlUriInput = input}></input>
                 </div>
-                <div className="form-group">
+                <div className="col-md-12">
+                <h2>Email Settings</h2><br/>
+                </div>
+                <div className="form-group col-md-6">
+                    <label>Email Domain:</label>
+                    <input className="form-control" type="text" defaultValue={this.state.data.emailDomain} id="emailDomain" name="emailDomain" ref={input => this.emailDomainInput = input}></input>
+                </div>
+                <div className="form-group col-md-6">
+                    <label>Registrar Email:</label>
+                    <input className="form-control" type="text" defaultValue={this.state.data.registrarEmail} id="registrarEmail" name="registrarEmail" ref={input => this.registrarEmailInput = input}></input>
+                </div>
+                <div className="form-group col-md-6">
+                    <label>Grad School Email:</label>
+                    <input className="form-control" type="text" defaultValue={this.state.data.gradSchoolEmail} id="gradSchoolEmail" name="gradSchoolEmail" ref={input => this.gradSchoolEmailInput = input}></input>
+                </div>
+                <div className="form-group col-md-6">
+                    <label>Background Check Email:</label>
+                    <input className="form-control" type="text" defaultValue={this.state.data.backgroundCheckEmail} id="backgroundCheckEmail" name="backgroundCheckEmail" ref={input => this.backgroundCheckEmailInput = input}></input>
+                </div>
+                <div className="form-group col-md-6">
+                    <label>Graduate Registrar Email:</label>
+                    <input className="form-control" type="text" defaultValue={this.state.data.graduateRegEmail} id="gradRegistrarEmail" name="gradRegistrarEmail" ref={input => this.gradRegistrarEmailInput = input}></input>
+                </div>
+                <div className="form-group col-md-6">
+                    <label>International Registrar Email:</label>
+                    <input className="form-control" type="text" defaultValue={this.state.data.internationalRegEmail} id="internationalRegistrarEmail" name="internationalRegistrarEmail" ref={input => this.internationalRegistrarEmailInput = input}></input>
+                </div>
+                <div className="form-group col-md-6">
+                    <label>Distance Education Email:</label>
+                    <input className="form-control" type="text" defaultValue={this.state.data.distanceEdEmail} id="distanceEdEmail" name="distanceEdEmail" ref={input => this.distanceEdEmailInput = input}></input>
+                </div>
+                <div className="form-group col-md-6">
+                    <label>International Office Email:</label>
+                    <input className="form-control" type="text" defaultValue={this.state.data.internationalOfficeEmail} id="internationalOfficeEmail" name="internationalOfficeEmail" ref={input => this.internationalOfficeEmailInput = input}></input>
+                </div>
+                <div className="form-group col-md-6">
                     <label>From Email:</label>
                     <input className="form-control" type="text" defaultValue={this.state.data.fromEmail} id="fromEmail" name="fromEmail" ref={input => this.fromEmailInput = input}></input>
+                </div>
+                <div className="form-group col-md-6">
+                    <label>Unusual Course Email:</label>
+                    <input className="form-control" type="text" defaultValue={this.state.data.unusualCourseEmail} id="unusualCourseEmail" name="unusualCourseEmail" ref={input => this.unusualCourseEmailInput = input}></input>
                 </div>
             </form>
 
             <div className="row">
-                <div className="col-sm-12 col-md-4 col-md-push-3">
+                <div className="col-sm-12 col-md-2 ">
                     {button}
                 </div>
             </div>
@@ -129,27 +184,7 @@ class Settings extends Component {
         )
     }
 }
-//
-// export default class SettingsRender extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <Settings name='systemName' />
-//                 <Settings name='registrarEmail' />
-//                 <Settings name='gradSchoolEmail' />
-//                 <Settings name='backgroundCheckEmail' />
-//                 <Settings name='graduateRegEmail' />
-//                 <Settings name='internationalRegEmail' />
-//                 <Settings name='distanceEdEmail' />
-//                 <Settings name='emailDomain' />
-//                 <Settings name='internationalOfficeEmail' />
-//                 <Settings name='wsdlUri' />
-//                 <Settings name='fromEmail' />
-//             </div>
-//         )
-//     }
-// }
-//
+
 ReactDOM.render(
     <Settings />,
     document.getElementById('content')
