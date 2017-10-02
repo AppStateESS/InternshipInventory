6905c2e7d1126eb33372d2daf9ffc77b7a7cc1e3
Jeremy Booker <jeremy.booker@gmail.com>
2017-10-02 10:58:13 -0400

    Declare majors array for all exec paths in TestMajorsProvider::getMajors()

--- class/DataProvider/Major/TestMajorsProvider.php
+++ class/DataProvider/Major/TestMajorsProvider.php
@@ -24,6 +24,8 @@ class TestMajorsProvider extends BannerMajorsProvider {
 
         $objs = array($m1, $m2, $m3, $m4, $m5, $m6);
 
+        $majors = array();
+
         foreach ($objs as $obj){
             $majors[] = new AcademicMajor($obj->major_code, $obj->major_desc, $obj->levl);
         }
