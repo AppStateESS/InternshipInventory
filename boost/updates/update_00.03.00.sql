0e5562822d403c9616df33b6f98821c3fd7a2a91
Matthew McNaney <mcnaney@gmail.com>
2018-04-10 09:03:17 -0400

    Fixed foreign key constraint update.

--- boost/updates/update_00.03.00.sql
+++ boost/updates/update_00.03.00.sql
@@ -7,8 +7,6 @@ CREATE TABLE intern_student_level(
 
 CREATE SEQUENCE intern_student_level_seq;
 
-ALTER TABLE intern_internship ADD CONSTRAINT code FOREIGN KEY (level) REFERENCES intern_student_level(code);
-
 INSERT INTO "intern_student_level" ("code", "description", "level") VALUES ('U', 'Undergraduate', 'ugrad');
 INSERT INTO "intern_student_level" ("code", "description", "level") VALUES ('G', 'Graduate', 'grad');
 INSERT INTO "intern_student_level" ("code", "description", "level") VALUES ('G2', 'Graduate 2', 'grad');
@@ -20,3 +18,5 @@ UPDATE "intern_internship" SET level = 'G' WHERE level = 'grad';
 UPDATE "intern_internship" SET level = 'G2' WHERE level = 'grad2';
 UPDATE "intern_internship" SET level = 'D' WHERE level = 'doctoral';
 UPDATE "intern_internship" SET level = 'P' WHERE level = 'postdoc';
+
+ALTER TABLE intern_internship ADD CONSTRAINT code FOREIGN KEY (level) REFERENCES intern_student_level(code);
