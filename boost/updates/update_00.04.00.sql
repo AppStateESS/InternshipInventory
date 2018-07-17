81cced40364fcfe2dfa623572a72981d3a865945
Matthew McNaney <mcnaney@gmail.com>
2018-07-17 08:50:02 -0400

    Added 0.5.0 update sql

--- boost/updates/update_00.04.00.sql
+++ boost/updates/update_00.04.00.sql
@@ -5,4 +5,3 @@ UPDATE intern_term SET undergrad_overload_hours=18 WHERE ((undergrad_overload_ho
 UPDATE intern_term SET undergrad_overload_hours=7 WHERE ((undergrad_overload_hours IS NULL) AND (semester_type=2 OR semester_type=3));
 UPDATE intern_term SET grad_overload_hours=12 WHERE ((grad_overload_hours IS NULL) AND (semester_type=1 OR semester_type=4));
 UPDATE intern_term SET grad_overload_hours=6 WHERE ((grad_overload_hours IS NULL) AND (semester_type=2 OR semester_type=3));
-
